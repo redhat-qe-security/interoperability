0edb80177dbfabdafa58d16ba856f97d09b9bcb2
Frantisek Sumsal <fsumsal@redhat.com>
2016-10-24 11:01:30 +0200

    Import changes from test repo #2
    - enable log folding
    - split test suites into separate environments
    - list executed/skipped/failed tests at the end of each job

--- scripts/test-runner.sh
+++ scripts/test-runner.sh
@@ -1,13 +1,31 @@
 #!/bin/bash
 
-if [[ $# < 2 ]]; then
+function fold_start() {
+    if [[ -z $1 ]]; then
+        echo >&2 "Fold: missing argument"
+    fi
+    echo -en "travis_fold:start:$1\\r"
+}
+
+function fold_end() {
+    if [[ -z $1 ]]; then
+        echo >&2 "Fold: missing argument"
+    fi
+    echo -en "travis_fold:end:$1\\r"
+}
+
+set +x
+
+if [[ $# < 3 ]]; then
     echo >&2 "Missing arguments"
     exit 1
 fi
 
 OS_TYPE="$1"
 OS_VERSION="$2"
+COMPONENT="$3"
 
+fold_start "machine-setup"
 yum -y makecache
 # epel-release is not available on fedora
 if [[ $OS_TYPE != "fedora" ]]; then
@@ -18,44 +36,71 @@ yum -y install openssl nss gnutls net-tools coreutils gawk \
                gnutls-utils expect make beakerlib findutils
 
 EC=0
+SKIP=0
+INDEX=0
 EXECUTED=()
 FAILED=()
+SKIPPED=()
 
 export PATH=${PATH}:/workspace/scripts
 
-while read test; do
+fold_end "machine-setup"
+
+# Just beautiful
+for test in $(find /workspace -type f ! -path "*/Library/*" \
+                              -path "*/$COMPONENT/*" -name "runtest.sh");
+do
+    ((INDEX++))
+    fold_start "runtest.sh.$INDEX"
+    SKIP=0
+
     echo "Running test: $test"
     pushd "$(dirname "$test")"
     if [[ ! -f Makefile ]]; then
         echo >&2 "Missing Makefile"
         EC=1
-        continue
+        SKIP=1
     fi
-    # Check relevancy
-    if ! relevancy.awk -v os_type=$OS_TYPE -v os_ver=$OS_VERSION Makefile; then
-        echo "This test is not relevant for current release"
-        continue
-    fi
-    # Install test dependencies
-    DEPS="$(awk '
-        match($0, /\"Requires:[[:space:]]*(.*)\"/, m) {
-            print m[1];
-        }' Makefile)"
-    if [[ ! -z $DEPS ]]; then
-        yum -y install $DEPS
-    fi
-    # Works only for beakerlib tests
-    make run
-    if [[ $? -ne 0 ]]; then
-        FAILED+=("$test")
-        EC=1
+    if [[ $SKIP -eq 0 ]]; then
+        # Check relevancy
+        if relevancy.awk -v os_type=$OS_TYPE -v os_ver=$OS_VERSION Makefile; then
+            # Install test dependencies
+            DEPS="$(awk '
+                match($0, /\"Requires:[[:space:]]*(.*)\"/, m) {
+                    print m[1];
+                }' Makefile)"
+            if [[ ! -z $DEPS ]]; then
+                yum -y install $DEPS
+            fi
+            # Works only for beakerlib tests
+            make run
+            if [[ $? -ne 0 ]]; then
+                FAILED+=("$test")
+                EC=1
+            fi
+        else
+            echo "This test is not relevant for current release"
+            SKIP=1
+        fi
     fi
     popd
-    EXECUTED+=("$test")
-done <<< "$(find /workspace -type f ! -path "*/Library/*" -name "runtest.sh")"
+
+    if [[ $SKIP -eq 0 ]]; then
+        EXECUTED+=("$test")
+    else
+        SKIPPED+=("$test")
+    fi
+
+    fold_end "runtest.sh.$INDEX"
+done
+
+echo "RESULTS:"
 
 echo "Executed tests:"
 printf '%s\n' "${EXECUTED[@]}"
+
+echo "Skipped tests:"
+printf '%s\n' "${SKIPPED[@]}"
 
 echo "Failed tests:"
 printf '%s\n' "${FAILED[@]}"
--- scripts/test-setup.sh
+++ scripts/test-setup.sh
@@ -25,8 +25,10 @@ mkdir -p "$LIB_PATH"
 echo "# library-prefix = fips" > "$LIB_PATH/lib.sh"
 echo "fipsLibraryLoaded() { return 0; }" >> "$LIB_PATH/lib.sh"
 
+RUNNER="/workspace/scripts/test-runner.sh"
+
 sudo docker run --rm --name "$CONT_NAME" \
                 -v $PWD:/workspace:rw \
                 ${OS_TYPE}:${OS_VERSION} \
                 /bin/bash -c \
-                "bash -x /workspace/scripts/test-runner.sh $OS_TYPE $OS_VERSION"
+                "bash -x $RUNNER $OS_TYPE $OS_VERSION $COMPONENT"
